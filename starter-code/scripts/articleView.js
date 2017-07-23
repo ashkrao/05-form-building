0e9fef67920c81640db05edf97f92e6f6af13542
Ashwini Rao <ashkrao@gmail.com>
2017-07-23 16:38:53 -0700

    Set up the JSON export functionality

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -119,9 +119,11 @@ articleView.create = function() {
   var html = compiledTemplate(newArticle);
   $('#articles').append(html);
 
-  // TODO: The new articles we create will be shown as JSON in an element in our article-export section. From there, we can copy/paste the JSON into our source data file.
+  // Done! TODO: The new articles we create will be shown as JSON in an element in our article-export section. From there, we can copy/paste the JSON into our source data file.
     // Set up this "export" functionality. When data is inputted into the form, that data should be converted to stringified JSON. Then, display that JSON in the element inside the article-export section. The article-export section was hidden on page load; make sure to show it as soon as data is entered in the form.
-
+  var jsonExport = JSON.stringify(newArticle);
+  $('#JSON-export').text(jsonExport);
+  $('#article-export').show();
 };
 
 
